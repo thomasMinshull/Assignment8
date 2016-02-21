19042d0c2dad84f1efab512b16e170dca7bb2435
Thomas Minshull <thomasminshull@gmail.com>
2016-02-21 17:29:59 -0400

    inital commit, pods added w/ OneSignal

---
+++ Assignment8/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Assignment8
+//
+//  Created by thomas minshull on 2016-02-18.
+//  Copyright © 2016 thomas minshull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
